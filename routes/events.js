66ee00de0cb38247ea0d010de265012c879e5d8a
Baenas <dbg94dani@gmail.com>
2020-03-17 20:40:02 +0100

    event update

--- routes/events.js
+++ routes/events.js
@@ -2,25 +2,38 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose')
 const Event = require('../models/events')
+
+router.use('/', function (req, res, next) {
+    if (req.cookies["datauser"]) {
+        let datausr = req.cookies["datauser"];
+        let name = datausr.username;
+
+        next()
+    } else {
+        res.redirect("/login");
+    }
+})
 router.get('/', function (req, res, next) {
     Event.find({}).then((data) => {
 
         res.render('events/eventshow', { data: data });
 
     })
+
 });
 router.get('/add', function (req, res, next) {
     res.render('events/eventadd');
 });
 router.post('/add', function (req, res, next) {
-
-    const { name, description, location, provincia, datefrom, dateto } = req.body;
+    let datausr = req.cookies["datauser"];
+    let admin = datausr.username;
+    const { name, description, location, provincia, datefrom, dateto, timefrom, timeto, price } = req.body;
 
     if (name == "" || description == "" || location == "") {
         res.render('events/eventadd', { error: "Empty" });
     } else {
 
-        Event.insertMany({ name: name, description: description, location: location, provincia: provincia, dateInit: datefrom, dateEnd: dateto })
+        Event.insertMany({ name: name, description: description, location: location, provincia: provincia, dateInit: datefrom, dateEnd: dateto, timeInit: timefrom, timeEnd: timeto, price: price, admin: admin })
         res.redirect('/events');
     }
 
@@ -36,13 +49,13 @@ router.get('/edit/:id/', function (req, res, next) {
 
 });
 router.post('/edit', function (req, res, next) {
-    const { name, description, location, id } = req.body;
+    const { name, description, location, id, datefrom, dateto, timefrom, timeto, price } = req.body;
 
     if (name == "" || description == "" || location == "") {
         res.render('events/eventedit', { error: "Empty" });
     } else {
 
-        Event.findByIdAndUpdate({ _id: id }, { name: name, description: description, location: location }).then((data) => {
+        Event.findByIdAndUpdate({ _id: id }, { name: name, description: description, location: location, dateInit: datefrom, dateEnd: dateto, timeInit: timefrom, timeEnd: timeto, price: price }).then((data) => {
             console.log(data)
             res.redirect('/events');
 
