5176a14d59a7b24128a46661d65f8c1dd7a9d565
msallito510 <msallito510@gmail.com>
2020-03-26 20:08:31 +0100

    update css

--- routes/events.js
+++ routes/events.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const mongoose = require("mongoose");
 const Event = require("../models/events");
 
-router.use("/", function (req, res, next) {
+router.use("/", function(req, res, next) {
   if (req.cookies["datauser"]) {
     let datausr = req.cookies["datauser"];
     let name = datausr.username;
@@ -14,7 +14,7 @@ router.use("/", function (req, res, next) {
   }
 });
 
-router.get("/", function (req, res, next) {
+router.get("/", function(req, res, next) {
   let datausr = req.cookies["datauser"];
   let name = datausr.username;
   Event.find({ admin: name }).then(persodata => {
@@ -24,11 +24,11 @@ router.get("/", function (req, res, next) {
   });
 });
 
-router.get("/add", function (req, res, next) {
+router.get("/add", function(req, res, next) {
   res.render("events/eventadd");
 });
 
-router.post("/add", function (req, res, next) {
+router.post("/add", function(req, res, next) {
   let datausr = req.cookies["datauser"];
   let admin = datausr.username;
   const {
@@ -62,43 +62,45 @@ router.post("/add", function (req, res, next) {
   }
 });
 
-router.get("/view/:id/", function (req, res, next) {
+router.get("/view/:id/", function(req, res, next) {
   let id = req.params.id;
   let datausr = req.cookies["datauser"];
   let usernow = datausr.username;
 
   Event.find({});
-  Event.findOne({ _id: id }).populate('members').then(data => {
-    if (datausr.role == "admin") {
-      res.render("events/eventview", {
-        data: data,
-        isAdmin: true,
-        canEdit: true
-      });
-    }
-    if (data.admin == usernow) {
-      res.render("events/eventview", {
-        data: data,
-        isAdmin: false,
-        canEdit: true
-      });
-    } else {
-      res.render("events/eventview", {
-        data: data,
-        isAdmin: false,
-        canEdit: false
-      });
-    }
-  });
+  Event.findOne({ _id: id })
+    .populate("members")
+    .then(data => {
+      if (datausr.role == "admin") {
+        res.render("events/eventview", {
+          data: data,
+          isAdmin: true,
+          canEdit: true
+        });
+      }
+      if (data.admin == usernow) {
+        res.render("events/eventview", {
+          data: data,
+          isAdmin: false,
+          canEdit: true
+        });
+      } else {
+        res.render("events/eventview", {
+          data: data,
+          isAdmin: false,
+          canEdit: false
+        });
+      }
+    });
 });
-router.get("/edit/:id/", function (req, res, next) {
+router.get("/edit/:id/", function(req, res, next) {
   let id = req.params.id;
   Event.findOne({ _id: id }).then(data => {
     res.render("events/eventedit", { data: data });
   });
 });
 
-router.post("/edit", function (req, res, next) {
+router.post("/edit", function(req, res, next) {
   const {
     name,
     description,
@@ -143,32 +145,25 @@ router.post("/edit", function (req, res, next) {
 //   });
 // });
 
-
-router.post("/cancel", function (req, res, next) {
+router.post("/remove/:id/", function(req, res, next) {
+  let id = req.params.id;
+  Event.findByIdAndRemove({ _id: id }).then(data => {
+    res.redirect("/events");
+  });
+});
+router.post("/cancel", function(req, res, next) {
   let datausr = req.cookies["datauser"];
   let usernow = datausr.username;
   let { id } = req.body;
   Event.findByIdAndUpdate(
     { _id: id },
-    { $pull: { members: datausr._id } }
+    { $pull: { guests: { username: usernow, id: datausr._id } } }
   ).then(() => {
     res.redirect("view/" + id + "/");
   });
 });
-router.get("/likes", function (req, res, next) {
 
-
-  let name = req.cookies["datauser"];
-  let userid = name._id
-
-  Event.find({ likes: userid }).populate('likes').then(data => {
-
-    res.render("events/eventLikes", { data: data, title: name });
-  });
-
-});
-
-router.post("/assist", function (req, res, next) {
+router.post("/assist", function(req, res, next) {
   let datausr = req.cookies["datauser"];
   let usernow = datausr.username;
   let { id } = req.body;
@@ -177,57 +172,45 @@ router.post("/assist", function (req, res, next) {
     username: usernow,
     id: datausr._id
   };
-  // Event.findOne({ _id: id, guests: { username: usernow, id: datausr._id } })
-  //   .then(data => {
-  //     if (!data) {
-  Event.findByIdAndUpdate(
-    { _id: id },
-    { members: datausr._id }
-  ).then(() => {
-    res.redirect("view/" + id + "/");
-  }).catch(() => {
-    res.redirect("view/" + id + "/");
-  });
-
-  //   } else {
-  //     res.redirect("view/" + id + "/");
-  //   }
-  // })
-
+  Event.findOne({ _id: id, guests: { username: usernow, id: datausr._id } })
+    .then(data => {
+      if (!data) {
+        Event.findByIdAndUpdate(
+          { _id: id },
+          { $push: { guests: { username: usernow, id: datausr._id } } }
+        ).then(() => {
+          res.redirect("view/" + id + "/");
+        });
+      } else {
+        res.redirect("view/" + id + "/");
+      }
+    })
+    .catch(() => {
+      res.redirect("view/" + id + "/");
+    });
 });
 
-router.post("/like/:id/", function (req, res, next) {
+router.get("/likes", function(req, res, next) {
+  if (req.cookies["datauser"]) {
+    let name = req.cookies["datauser"];
+    let id = req.params.id;
+    Event.find({}).then(data => {
+      res.render("events/eventLikes", { data: data, title: name });
+    });
+  }
+});
+
+router.post("/like/:id/", function(req, res, next) {
   let id = req.params.id;
   let name = req.cookies["datauser"];
-  iduser = name._id
 
-  Event.findById({ _id: id }, { likes: iduser }).populate('likes').then((data) => {
-    console.log(data.likes)
-    if (data.likes.length == 0) {
-      Event.findByIdAndUpdate(
-        { _id: id },
-        { likes: iduser }
-      ).then(() => {
-
-        res.redirect('/')
-      })
-    } else {
-      Event.findByIdAndUpdate({ _id: id }, { $pull: { likes: iduser } }).then((data) => {
-        console.log(data)
-        res.redirect('/')
-      })
+  Event.insertOne({
+    likes: {
+      id: id
     }
-  })
-
-});
-
-router.post("/remove/:id/", function (req, res, next) {
-  let id = req.params.id;
-  Event.findByIdAndRemove({ _id: id }).then(data => {
-    res.redirect("/events");
+  }).then(data => {
+    res.redirect("/events/eventLikes");
   });
 });
-
-
 
 module.exports = router;
